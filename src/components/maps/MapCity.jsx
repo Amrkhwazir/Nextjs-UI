fec4fc1e7a9847bc5e6f4b2b8e8c7447ae00bad6
Amrkhwazir <amrkh5678@gmail.com>
2024-06-25 13:33:55 +0500

    working on responsive

--- src/components/maps/MapCity.jsx
+++ src/components/maps/MapCity.jsx
@@ -7,7 +7,7 @@ const MapCity = () => {
   return (
     <Container sx={{marginTop: "20px", backgroundColor: "white", padding: "40px", borderRadius: '12px', minWidth: '100%', display: "flex", flexDirection: "column", alignItems: "center"}}>
 
-          <Box sx={{width: "800px", display: "flex", justifyContent: "flex-start", marginBottom: '10px', width: '100%'}}>
+          <Box sx={{minWidth: "90%", display: "flex", justifyContent: "flex-start", marginBottom: '10px', }}>
             <Box sx={{display: "flex", paddingLeft: "12px",gap: "10px", alignItems: "center"}}>
             <Image src={"/images/navigate.png"} width={40} height={40} />
             <h4>Location History</h4>
@@ -22,7 +22,7 @@ const MapCity = () => {
                 <h3>City Center</h3>
             </Box>
         </Box>
-        <Container sx={{marginTop: "20px", display: "flex", justifyContent: {xs: "center", sm: 'flex-end', md: 'flex-end', lg: 'flex-end', xl: 'flex-end'}, gap: '10px', width: {xs: "100%", sm: "90%", md: "100%", lg: "90%"}}}>
+        <Container sx={{marginTop: "20px", display: "flex", justifyContent: {xs: "center", sm: 'flex-end', md: 'flex-end', lg: 'flex-end', xl: 'flex-end'}, gap: '10px', width: {xs: "100%", sm: "100%", md: "100%", lg: "80%"}}}>
         <Button sx={{borderRadius: "15px", backgroundColor: "#42A9F4", color: "white", height: "30px", fontSize: {xs: "9px", sm: "12px", md: '14px', lg: "14px", xl : "16px"}, maxWidth: "120px"}}>This Week</Button>
         <Button sx={{borderRadius: "15px", backgroundColor: "whitesmoke", color: "grey", height: "30px", fontSize: {xs: "8px", sm: "12px", md: '14px', lg: "14px", xl : "16px"}, maxWidth: "120px"}}>This Month</Button>
         <Button sx={{borderRadius: "15px", backgroundColor: "whitesmoke", color: "grey", height: "30px", fontSize: {xs: "9px", sm: "12px", md: '14px', lg: "14px", xl : "16px"}, maxWidth: "120px"}}>Today</Button>
